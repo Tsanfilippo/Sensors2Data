b142e19efc2f1f19293bef91ba1eec0c17aaab77
Dave Warner <dmwarner@usgs.gov>
2019-08-19 02:24:02 -0400

    Update NewData.R

--- R/NewData.R
+++ R/NewData.R
@@ -36,18 +36,20 @@ NewData<- function(){
   df$Vessel <- dlg_input(GUI =Vessel,"Enter vessel as number")$res
   warp.fdepth.dat <- rbind(warp.fdepth.dat, df)
   write.csv(warp.fdepth.dat, paste0(rdat), row.names=FALSE)
-  plot(Cable.feet ~ Depth.meters, data=warp.fdepth.dat )
+
   fit <- lm(Cable.feet ~ Depth.meters, data=warp.fdepth.dat)
-  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) +
+  p1 <- ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) +
     geom_point() +
     stat_smooth(method = "lm", col = "magenta") +
     labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
                        "Intercept =",signif(fit$coef[[1]],5 ),
                        " Slope =",signif(fit$coef[[2]], 5),
                        " P =",signif(summary(fit)$coef[2,4], 5)))
-  ggplot(fit, aes(fit$fitted.values, fit$residuals)) +
+  print(p1)
+  p2 <- ggplot(fit, aes(fit$fitted.values, fit$residuals)) +
     geom_point() +
     labs(x = "Fitted values (ft)", y="Residuas", size=16)
+  print(p2)
   }
 
 
