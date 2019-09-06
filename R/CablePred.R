949a01b6c957bb6c7c536f498ae80b2e7fb6e592
Dave Warner <dmwarner@usgs.gov>
2019-09-06 10:19:30 -0400

    Adjustments to function.

--- R/CablePred.R
+++ R/CablePred.R
@@ -23,7 +23,7 @@
 #' @export
 #'
 
-CablePred <- function(desired.depth) {
+CablePred <- function(desired.depth = 10) {
   rdat <- dlg_open(title = "Select data file with cable length and fishing depth data.",
                    filters = dlg_filters[c("R", "All"), ])$res
   dat <- read.csv (rdat, header=TRUE)
@@ -31,7 +31,7 @@ CablePred <- function(desired.depth) {
   cable.lm <- lm(Cable.feet ~ Depth.meters, data = dat)
   #desired.depth <- data.frame(Depth.meters = as.numeric(dlg_input(GUI =depth,"Enter desired fishing depth in meters")$res))
   cable.required <- predict.lm(cable.lm, newdata = desired.depth, interval = "confidence")
-  #cable.required <- as.numeric(coef(cable.lm)[1] + coef(cable.lm)[2]*as.numeric(desired.depth))
+  cable.required <- as.numeric(coef(cable.lm)[1] + coef(cable.lm)[2]*as.numeric(desired.depth))
   cable.required2 <- paste0("Estimated cable required = ", round(cable.required[1]), " ft,  ", " range = ", round(cable.required[2]),
                             " - ", round(cable.required[3]), " ft")
   return(cable.required2)
